45e9b1aec2ffebd97c8debb1e781a7c04211b81d
Cody Averett <codyaverett@gmail.com>
2022-10-31 07:06:06 +0000

    feat(database): sql scripts for data genesis and timeseries queries on price

---
+++ src/database/sql/core/price_moving_averages.sql
@@ -0,0 +1,18 @@
+
+-- Windows at the earliest times will have a smaller average aggregate counts 
+-- due to lack of data to calculate the average.
+
+SELECT time
+	, price
+	, AVG(price) OVER(ORDER BY time
+      ROWS BETWEEN 10 PRECEDING AND CURRENT ROW)
+    	AS moving_average_11
+	, AVG(price) OVER(ORDER BY time
+      	ROWS BETWEEN 20 PRECEDING AND CURRENT ROW)
+    	AS moving_average_21
+	, AVG(price) OVER(ORDER BY time
+      	ROWS BETWEEN 49 PRECEDING AND CURRENT ROW)
+    	AS moving_average_50
+  FROM public.core_price
+  WHERE pair_id = 1 and time > NOW() - INTERVAL '1 day'
+  ORDER BY time DESC;
