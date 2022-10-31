7a4d34560688e19832d32576d9d81483cc7545cf
Cody Averett <codyaverett@gmail.com>
2022-10-31 07:08:28 +0000

    feat(airflow): price fetch dag, and localexecutor config

---
+++ src/airflow/dags/eth_prices.py
@@ -0,0 +1,53 @@
+from airflow.models import Variable
+from airflow.decorators import dag, task
+from etherscan import Etherscan
+from datetime import datetime, timedelta
+import requests
+
+eth = Etherscan(Variable.get("ETHERSCAN_KEY2", "")) # key in quotation marks
+serviceAPI = Variable.get("SERVICE_API", "") # key in quotation marks
+
+
+@dag(
+    schedule="* * * * *",
+    start_date=datetime(2022, 10, 29),
+    catchup=False,
+    default_args={
+        "retries": 2
+    },
+    max_active_runs=1,
+    dagrun_timeout=timedelta(seconds=50),
+    tags=['price'])
+def eth_prices_dag():
+    """
+    This DAG simply retrieves and stores the current ETH price in USD and BTC.
+    """
+    
+    @task()
+    def get_latest_eth_prices():
+        return eth.get_eth_last_price()
+    
+    @task()
+    def log_eth_prices(prices):
+        print(prices)
+        r1 = requests.post(serviceAPI + "price/", 
+            json={"pair": 1, 
+                  "price": float(prices["ethusd"]),
+                  "time": datetime
+                    .utcfromtimestamp(int(prices["ethusd_timestamp"]))
+                    .strftime('%Y-%m-%dT%H:%M:%S')})
+        r2 = requests.post(serviceAPI + "price/", 
+            json={"pair": 2, 
+                  "price": float(prices["ethbtc"]),
+                  "time": datetime
+                    .utcfromtimestamp(int(prices["ethbtc_timestamp"]))
+                    .strftime('%Y-%m-%dT%H:%M:%S')})
+        print(r1.text)
+        print(r2.text)
+        
+    # Call the task functions to infer dependencies
+    prices = get_latest_eth_prices()
+    log_eth_prices(prices)
+
+# Call the dag function to register the DAG
+eth_prices_dag = eth_prices_dag()
